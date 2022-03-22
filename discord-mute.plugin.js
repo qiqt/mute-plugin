fb74973bd195cb041eb2c91b6c2976fcc019fdaa
qiqt <71264687+qiqt@users.noreply.github.com>
2022-03-22 14:09:56 +0200

    first commit

---
+++ discord-mute.plugin.js
@@ -0,0 +1,22 @@
+/**
+ * @name Fast Mute
+ * @version 0.0.1
+ * @description Example Description
+ * @author qiqt
+ *  
+ */
+
+const request = require("request");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class FastMute {
+
+  load() { }
+  start() {
+    
+  }
+  stop() {
+
+  }
+}
